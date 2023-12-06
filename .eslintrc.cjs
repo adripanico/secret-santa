401462bfcac4a106698c8aa28c19d57f58a53613
Adrian Caballero <adripanicodev@gmail.com>
2023-12-06 12:45:59 +0100

    chore: eslint and prettier configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,15 +4,68 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:prettier/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:typescript-sort-keys/recommended',
+    'plugin:i18next/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier', 'simple-import-sort', 'import', 'unused-imports'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'prettier/prettier': 'error',
+    curly: ['error', 'all'],
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          // Packages.
+          // Things that start with a letter (or digit or underscore), or `@` followed by a letter.
+          ['^@?\\w'],
+          // Absolute imports and other imports such as Vue-style `@/foo`.
+          // Anything not matched in another group.
+          ['^'],
+          // Relative imports.
+          // Anything that starts with a dot.
+          ['^\\.'],
+          // Side effect imports.
+          ['^\\u0000'],
+        ],
+      },
     ],
+    'simple-import-sort/exports': 'error',
+    'import/no-anonymous-default-export': 'off',
+    'import/first': 'error',
+    'import/no-unresolved': ['error', { ignore: ['.(scss|less|css)$', 'environment'] }],
+    'import/newline-after-import': 'off',
+    'import/no-duplicates': 'error',
   },
-}
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+      },
+      /* Alias definitions. Also in tsconfig.json and vite.config.ts. */
+      alias: {
+        map: [
+          ['assets', './src/assets'],
+          ['components', './src/components'],
+          ['hooks', './src/hooks'],
+          ['i18n', './src/i18n'],
+          ['models', './src/models'],
+          ['store', './src/store'],
+          ['utils', './src/utils'],
+        ],
+        extensions: ['.ts', '.js', '.tsx', '.json'],
+      },
+    },
+  },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    eslintPlugin({
+      cache: false,
+      failOnWarning: false,
+      failOnError: true,
+    }),
+  ],
+  resolve: {
+    /* Alias definitions. Also in tsconfig.json and .eslintrc.cjs. */
+    alias: {
+      assets: `${path.resolve(__dirname, './src/assets/')}`,
+      components: `${path.resolve(__dirname, './src/components/')}`,
+      hooks: `${path.resolve(__dirname, './src/hooks/')}`,
+      i18n: `${path.resolve(__dirname, './src/i18n/')}`,
+      models: `${path.resolve(__dirname, './src/models/')}`,
+      store: `${path.resolve(__dirname, './src/store/')}`,
+      utils: `${path.resolve(__dirname, './src/utils/')}`,
+    },
+  },
+});
